a87c48087470987427fe4b3f72fe5f08b4418268
panachai <panachai.ny@gmail.com>
2017-05-25 17:29:10 +0700

    complete

--- LearnLibSignature/MainActivity.cs
+++ LearnLibSignature/MainActivity.cs
@@ -10,13 +10,28 @@ using Xamarin.Controls;
 namespace LearnLibSignature {
 	[Activity(Label = "LearnLibSignature", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity {
+		CustomViewSignature customViewSignature;
+
 		protected override void OnCreate(Bundle savedInstanceState) {
 			base.OnCreate(savedInstanceState);
 			SetContentView(Resource.Layout.Main);
 
-
-
+			Init();
+			//customViewSignature.BackgroundImageResource = Resource.Drawable.logo_galaxy_black_64;
+			Setting();
 		}
+
+		void Init() {
+			//customViewSignature = new CustomViewSignature(this);
+			customViewSignature = FindViewById<CustomViewSignature>(Resource.Id.csvSearchEdittext);
+		}
+
+		void Setting() {
+			//customViewSignature.BackgroundImageResource = Resource.Drawable.logo_galaxy_black_64;
+
+			customViewSignature.StrokeColor = Color.Red;
+		}
+
 	}
 }
 
